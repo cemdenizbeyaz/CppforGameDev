b4d33c75ade68c6da19dd94278cf9ac0f9c6da1b
Cem Deniz Beyaz <cemdenizbeyaz1@gmail.com>
2023-05-27 11:17:22 +0300

    tutorial5 added

---
+++ Tutorial5.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int myInt(0);
+
+    int count = 0;
+
+    while (count <= 10)
+    {
+        cout << myInt << endl;
+        myInt++;
+        count++;
+    }
+
+    double NumberPi = 3.14159;
+    double NumberE = 2.718281828;
+
+    int counter = 0;
+
+    bool Condition = true;
+    
+    do
+    {
+        cout << "Number Pi is: " << NumberPi << endl;
+        cout << "Number E is: " << NumberE << endl;
+        cout << "Counter is: " << counter << endl;
+        cout << "Counter * (E + Pi) is:" << counter * (NumberE + NumberPi) << endl;
+
+        counter++;
+        if (counter <= 10)
+        {
+            Condition = true;
+        }
+        else
+        {
+            Condition = false;
+        }
+    } while (Condition);
+    
+
+    system("pause");
+}
